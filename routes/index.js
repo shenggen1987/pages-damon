bd4b07a249413f6535c0be61a8c07cdee38eeef6
lishenggen <lishenggen@qjdchina.com>
2017-04-14 11:02:43 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var router = require('koa-router')();
+
+router.get('/', async function (ctx, next) {
+  ctx.state = {
+    title: 'koa2 title',
+    name: '231231'
+  };
+  
+  await ctx.render('index', {
+    title: "Test Page",
+    name: "World"
+  });
+})
+module.exports = router;
