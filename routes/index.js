d031923b4a038eb79e27a338fe475a70234f5f38
lishenggen <lishenggen@qjdchina.com>
2017-04-25 11:58:27 +0800

    detail

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var router = require('koa-router')();
-
+var data = require('../lib/data');
 router.get('/', async function (ctx, next) {
+	var list = data.getData;
+	list.forEach(function(item){
+		if(item.id < 10){
+			item.indexNo = '0' + item.id;
+		}else{
+			item.indexNo = '' + item.id;
+		}
+	})
   ctx.state = {
     title: 'koa2 title',
     name: '231231',
-    pageName: 'index'
+    pageName: 'index',
+    list: list
   };
 
   await ctx.render('index', {
