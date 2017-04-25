9907fd200cbbdc5e48e45e6af6791bb7b51f114c
lishenggen <lishenggen@qjdchina.com>
2017-04-25 15:57:07 +0800

    detail

--- routes/detail.js
+++ routes/detail.js
@@ -7,6 +7,19 @@ router.get('/:id', async function (ctx, next) {
 	arr.forEach(function(item){
 		if(item.id === ~~ctx.params.id){
 			detail = item;
+		  var relatedItem = {};
+		  detail.relatedList = [];
+			detail.related.forEach(function(dd, index){
+				var aa = getItem(arr, dd);
+				relatedItem = {
+					first: index % 3 === 0 ? 1 : 0 ,
+					last: (index + 1) % 3 === 0 ? 1 : 0,
+					title: aa.title,
+					service: aa.service,
+					id: aa.id
+				}
+				detail.relatedList.push(relatedItem);
+			})
 		}
 	});
   ctx.state = {
@@ -25,3 +38,13 @@ router.get('/:id', async function (ctx, next) {
 });
 
 module.exports = router;
+
+function getItem(items, id){
+	var item;
+	items.forEach(function(tmp){
+		if(tmp.id === id){
+			item = tmp;
+		}
+	});
+	return item;
+}
