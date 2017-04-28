83c4ce41b7d5782ae8b92f4349e6e88fe5323050
lishenggen <lishenggen@qjdchina.com>
2017-04-28 10:43:04 +0800

    proto

--- routes/cases.js
+++ routes/cases.js
@@ -2,13 +2,23 @@ var router = require('koa-router')();
 var data = require('../lib/data');
 router.get('/', async function (ctx, next) {
 	var list = data.getData;
+  var category = ctx.query.category;
+  console.log(ctx.query.category);
 	list.forEach(function(item){
 		if(item.id < 10){
 			item.indexNo = '0' + item.id;
 		}else{
 			item.indexNo = '' + item.id;
 		}
+    if(!item.category){
+      item.category = 1;
+    }
 	})
+  if(category){
+    list = list.filter(function(item){
+      return item.category == category;
+    });
+  }
   ctx.state = {
     title: 'koa2 title',
     name: '231231',
